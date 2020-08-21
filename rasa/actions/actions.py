5b9b9a8105bc7c6a05e8b6697adf3ef0a7f55fed
Luca Gioppo <gioppoluca@libero.it>
2020-08-21 17:32:46 +0200

    updated chatbot with development as long as we do not create a proper data image also for the bot

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -3,145 +3,10 @@ from typing import Dict, Text, Any, List, Union, Optional
 from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
+import requests
+import logging
 
-
-class RestaurantForm(FormAction):
-    """Example of a custom form action"""
-
-    def name(self) -> Text:
-        """Unique identifier of the form"""
-
-        return "restaurant_form"
-
-    @staticmethod
-    def required_slots(tracker: Tracker) -> List[Text]:
-        """A list of required slots that the form has to fill"""
-
-        return ["cuisine", "num_people", "outdoor_seating", "preferences", "feedback"]
-
-    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-        """A dictionary to map required slots to
-            - an extracted entity
-            - intent: value pairs
-            - a whole message
-            or a list of them, where a first match will be picked"""
-
-        return {
-            "cuisine": self.from_entity(entity="cuisine", not_intent="chitchat"),
-            "num_people": [
-                self.from_entity(
-                    entity="number", intent=["inform", "request_restaurant"]
-                ),
-            ],
-            "outdoor_seating": [
-                self.from_entity(entity="seating"),
-                self.from_intent(intent="affirm", value=True),
-                self.from_intent(intent="deny", value=False),
-            ],
-            "preferences": [
-                self.from_intent(intent="deny", value="no additional preferences"),
-                self.from_text(not_intent="affirm"),
-            ],
-            "feedback": [self.from_entity(entity="feedback"), self.from_text()],
-        }
-
-    # USED FOR DOCS: do not rename without updating in docs
-    @staticmethod
-    def cuisine_db() -> List[Text]:
-        """Database of supported cuisines"""
-
-        return [
-            "caribbean",
-            "chinese",
-            "french",
-            "greek",
-            "indian",
-            "italian",
-            "mexican",
-        ]
-
-    @staticmethod
-    def is_int(string: Text) -> bool:
-        """Check if a string is an integer"""
-
-        try:
-            int(string)
-            return True
-        except ValueError:
-            return False
-
-    # USED FOR DOCS: do not rename without updating in docs
-    def validate_cuisine(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        """Validate cuisine value."""
-
-        if value.lower() in self.cuisine_db():
-            # validation succeeded, set the value of the "cuisine" slot to value
-            return {"cuisine": value}
-        else:
-            dispatcher.utter_message(template="utter_wrong_cuisine")
-            # validation failed, set this slot to None, meaning the
-            # user will be asked for the slot again
-            return {"cuisine": None}
-
-    def validate_num_people(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        """Validate num_people value."""
-
-        if self.is_int(value) and int(value) > 0:
-            return {"num_people": value}
-        else:
-            dispatcher.utter_message(template="utter_wrong_num_people")
-            # validation failed, set slot to None
-            return {"num_people": None}
-
-    def validate_outdoor_seating(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        """Validate outdoor_seating value."""
-
-        if isinstance(value, str):
-            if "out" in value:
-                # convert "out..." to True
-                return {"outdoor_seating": True}
-            elif "in" in value:
-                # convert "in..." to False
-                return {"outdoor_seating": False}
-            else:
-                dispatcher.utter_message(template="utter_wrong_outdoor_seating")
-                # validation failed, set slot to None
-                return {"outdoor_seating": None}
-
-        else:
-            # affirm/deny was picked up as T/F
-            return {"outdoor_seating": value}
-
-    def submit(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict]:
-        """Define what the form has to do
-            after all required slots are filled"""
-
-        # utter submit template
-        dispatcher.utter_message(template="utter_submit")
-        return []
+logger = logging.getLogger(__name__)
 
 class DocumentForm(FormAction):
     """Example of a custom form action"""
@@ -175,6 +40,7 @@ class DocumentForm(FormAction):
 
         return [
             "residence permit",
+            "stamp duty",
             "identity card",
         ]
 
@@ -207,8 +73,23 @@ class DocumentForm(FormAction):
     ) -> List[Dict]:
         """Define what the form has to do
             after all required slots are filled"""
+        
+        lang = "en"
+        document = tracker.get_slot("document")
+        url = "https://api.micadoproject.eu/backend/1.0.0/process_x_doc"
 
+        logger.info(tracker)
+        logger.info(url)
+        
+        headers = {"Prefer": "resolution=merge-duplicates","apikey": "eyJ4NXQiOiJaalJtWVRNd05USmpPV1U1TW1Jek1qZ3pOREkzWTJJeU1tSXlZMkV6TWpkaFpqVmlNamMwWmc9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImFwcGxpY2F0aW9uIjp7Im93bmVyIjoiYWRtaW4iLCJ0aWVyIjoiVW5saW1pdGVkIiwibmFtZSI6IkRlZmF1bHRBcHBsaWNhdGlvbiIsImlkIjoxLCJ1dWlkIjoiNzBmNGQ5MzEtNmQxYi00ZGJiLThiZGItNjgyNGViZWRhNjA5In0sInRpZXJJbmZvIjp7IlVubGltaXRlZCI6eyJzdG9wT25RdW90YVJlYWNoIjp0cnVlLCJzcGlrZUFycmVzdExpbWl0IjowLCJzcGlrZUFycmVzdFVuaXQiOm51bGx9fSwia2V5dHlwZSI6IlBST0RVQ1RJT04iLCJzdWJzY3JpYmVkQVBJcyI6W3sic3Vic2NyaWJlclRlbmFudERvbWFpbiI6ImNhcmJvbi5zdXBlciIsIm5hbWUiOiJQb3N0Z1JFU1RBUEkiLCJjb250ZXh0IjoiXC9kYlwvdjAxIiwicHVibGlzaGVyIjoiYWRtaW4iLCJ2ZXJzaW9uIjoidjAxIiwic3Vic2NyaXB0aW9uVGllciI6IlVubGltaXRlZCJ9LHsic3Vic2NyaWJlclRlbmFudERvbWFpbiI6ImNhcmJvbi5zdXBlciIsIm5hbWUiOiJtaWNhZG9fYmFja2VuZCIsImNvbnRleHQiOiJcL2JhY2tlbmRcLzEuMC4wIiwicHVibGlzaGVyIjoiYWRtaW4iLCJ2ZXJzaW9uIjoiMS4wLjAiLCJzdWJzY3JpcHRpb25UaWVyIjoiVW5saW1pdGVkIn0seyJzdWJzY3JpYmVyVGVuYW50RG9tYWluIjoiY2FyYm9uLnN1cGVyIiwibmFtZSI6ImNoYXRib3QiLCJjb250ZXh0IjoiXC9ib3RcL3YwMSIsInB1Ymxpc2hlciI6ImFkbWluIiwidmVyc2lvbiI6InYwMSIsInN1YnNjcmlwdGlvblRpZXIiOiJVbmxpbWl0ZWQifV0sImlhdCI6MTU5MjkyMDczMCwianRpIjoiMmZmYzQ3ZTMtMDZkYi00NGIxLWE1NmEtMDhhMjdmOTkyY2JlIn0=.Ud-i7sAF3B915zglltrumtL8TXnojdBEpamYqk1tz4qhzQEyj6UOkrZJELA3C4Czn8x0tFlBEeNz23v6deGs8g_KZRHXrW6mYBH7bRZ-Fbp6yntPhgaUr-e28cTZZlHmOwB0256e1uEFWTnZvjSvomhJHOTi2Zl1fO89So3uHkWA9qYmAU77Z5qz-GlYvDq2G6VTTtYZLIvJ8fkICj53tN_TH6VLHBEIExNtDwV5m4yTMsoxrsHuIuLMNgpczx3ejwk4b81NobJgE2K3Uq7ihKAHo8tZ10qX91q8qDRPVXfjLQE7qTmoOKLPrhXsE3pvuMJyMpESmEn0oKOaphs9Og=="}
+        params=[('lang','en'), ('document',document)]
+        response_dict = requests.get(url, headers=headers, params=params)
+
+        # logger.info(response_dict.headers)
+        logger.info(response_dict.text)
+        logger.info(response_dict.request)
+        logger.info(response_dict)
         # utter submit template
-        dispatcher.utter_message(template="utter_submit")
+        dispatcher.utter_message(channel="micado", custom=response_dict.text)
         return []
 
--- rasa/data/stories.md
+++ rasa/data/stories.md
@@ -1,151 +1,160 @@
+## happy path short
+* greet
+    - utter_greet
+* request_document
+    - document_form
+    - form{"name": "document_form"}
+    - form{"name": null}
+    - action_restart
+
 ## happy path
 * greet
     - utter_greet
-* request_restaurant
-    - restaurant_form
-    - form{"name": "restaurant_form"}
+* request_document
+    - document_form
+    - form{"name": "document_form"}
     - form{"name": null}
-    - utter_slots_values
 * thankyou
     - utter_noworries
+    - action_restart
 
 ## unhappy path
 * greet
     - utter_greet
-* request_restaurant
-    - restaurant_form
-    - form{"name": "restaurant_form"}
+* request_document
+    - document_form
+    - form{"name": "document_form"}
 * chitchat
     - utter_chitchat
-    - restaurant_form
+    - document_form
     - form{"name": null}
-    - utter_slots_values
+    - utter_slots_values_document
 * thankyou
     - utter_noworries
+    - action_restart
 
 ## very unhappy path
 * greet
     - utter_greet
 * chitchat
     - utter_chitchat
-    - restaurant_form
-* chitchat
-    - utter_chitchat
-    - restaurant_form
-* chitchat
-    - utter_chitchat
-    - restaurant_form
-    - form{"name": null}
-    - utter_slots_values
-* thankyou
-    - utter_noworries
-
-## stop but continue path
-* greet
-    - utter_greet
-* request_restaurant
-    - restaurant_form
-    - form{"name": "restaurant_form"}
-* stop
-    - utter_ask_continue
-* affirm
-    - restaurant_form
-    - form{"name": null}
-    - utter_slots_values
-* thankyou
-    - utter_noworries
-
-## stop and really stop path
-* greet
-    - utter_greet
-* request_restaurant
-    - restaurant_form
-    - form{"name": "restaurant_form"}
-* stop
-    - utter_ask_continue
-* deny
-    - action_deactivate_form
-    - form{"name": null}
-
-## chitchat stop but continue path
-* request_restaurant
-    - restaurant_form
-    - form{"name": "restaurant_form"}
-* chitchat
-    - utter_chitchat
-    - restaurant_form
-* stop
-    - utter_ask_continue
-* affirm
-    - restaurant_form
-    - form{"name": null}
-    - utter_slots_values
-* thankyou
-    - utter_noworries
-
-## stop but continue and chitchat path
-* greet
-    - utter_greet
-* request_restaurant
-    - restaurant_form
-    - form{"name": "restaurant_form"}
-* stop
-    - utter_ask_continue
-* affirm
-    - restaurant_form
-* chitchat
-    - utter_chitchat
-    - restaurant_form
-    - form{"name": null}
-    - utter_slots_values
-* thankyou
-    - utter_noworries
-
-## chitchat stop but continue and chitchat path
-* greet
-    - utter_greet
-* request_restaurant
-    - restaurant_form
-    - form{"name": "restaurant_form"}
-* chitchat
-    - utter_chitchat
-    - restaurant_form
-* stop
-    - utter_ask_continue
-* affirm
-    - restaurant_form
-* chitchat
-    - utter_chitchat
-    - restaurant_form
-    - form{"name": null}
-    - utter_slots_values
-* thankyou
-    - utter_noworries
-
-## chitchat, stop and really stop path
-* greet
-    - utter_greet
-* request_restaurant
-    - restaurant_form
-    - form{"name": "restaurant_form"}
-* chitchat
-    - utter_chitchat
-    - restaurant_form
-* stop
-    - utter_ask_continue
-* deny
-    - action_deactivate_form
-    - form{"name": null}
-
-## bot challenge
-* bot_challenge
-  - utter_iamabot
-
-## document micado
-* request_document
     - document_form
-    - form{"name": "document_form"}
+* chitchat
+    - utter_chitchat
+    - document_form
+* chitchat
+    - utter_chitchat
+    - document_form
     - form{"name": null}
     - utter_slots_values_document
 * thankyou
     - utter_noworries
+    - action_restart
+
+## stop but continue path
+* greet
+    - utter_greet
+* request_document
+    - document_form
+    - form{"name": "document_form"}
+* stop
+    - utter_ask_continue
+* affirm
+    - document_form
+    - form{"name": null}
+    - utter_slots_values_document
+* thankyou
+    - utter_noworries
+    - action_restart
+
+## stop and really stop path
+* greet
+    - utter_greet
+* request_document
+    - document_form
+    - form{"name": "document_form"}
+* stop
+    - utter_ask_continue
+* deny
+    - action_deactivate_form
+    - form{"name": null}
+    - action_restart
+
+## chitchat stop but continue path
+* request_document
+    - document_form
+    - form{"name": "document_form"}
+* chitchat
+    - utter_chitchat
+    - document_form
+* stop
+    - utter_ask_continue
+* affirm
+    - document_form
+    - form{"name": null}
+    - utter_slots_values_document
+* thankyou
+    - utter_noworries
+    - action_restart
+
+## stop but continue and chitchat path
+* greet
+    - utter_greet
+* request_document
+    - document_form
+    - form{"name": "document_form"}
+* stop
+    - utter_ask_continue
+* affirm
+    - document_form
+* chitchat
+    - utter_chitchat
+    - document_form
+    - form{"name": null}
+    - utter_slots_values_document
+* thankyou
+    - utter_noworries
+    - action_restart
+
+## chitchat stop but continue and chitchat path
+* greet
+    - utter_greet
+* request_document
+    - document_form
+    - form{"name": "document_form"}
+* chitchat
+    - utter_chitchat
+    - document_form
+* stop
+    - utter_ask_continue
+* affirm
+    - document_form
+* chitchat
+    - utter_chitchat
+    - document_form
+    - form{"name": null}
+    - utter_slots_values_document
+* thankyou
+    - utter_noworries
+    - action_restart
+
+## chitchat, stop and really stop path
+* greet
+    - utter_greet
+* request_document
+    - document_form
+    - form{"name": "document_form"}
+* chitchat
+    - utter_chitchat
+    - document_form
+* stop
+    - utter_ask_continue
+* deny
+    - action_deactivate_form
+    - form{"name": null}
+    - action_restart
+
+## bot challenge
+* bot_challenge
+  - utter_iamabot
+    - action_restart
