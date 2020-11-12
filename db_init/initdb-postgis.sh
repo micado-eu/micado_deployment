a8d7c97708fa25f692160ff910f8291839cb772d
Luca Gioppo <gioppoluca@libero.it>
2020-11-12 09:22:56 +0100

    fixing typo

--- db_init/initdb-postgis.sh
+++ db_init/initdb-postgis.sh
@@ -12,8 +12,8 @@ EOSQL
 
 # Load PostGIS into both template_database and $POSTGRES_DB
 for DB in template_postgis "$POSTGRES_DB"; do
-	echo "Loading PostGIS extensions into $DB"
-	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
+	echo "Loading PostGIS extensions into $POSTGRES_DB"
+	"${psql[@]}" --dbname="$POSTGRES_DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS postgis;
 		CREATE EXTENSION IF NOT EXISTS postgis_topology;
 		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
