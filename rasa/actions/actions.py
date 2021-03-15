8970b48fccc7c6f2794b1c85a57a23a7c2434053
Luca Gioppo <gioppoluca@libero.it>
2021-03-15 12:13:56 +0100

    logging

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -8,6 +8,15 @@ import logging
 import os
 
 logger = logging.getLogger(__name__)
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+logger.addHandler(handler)
 
 class DocumentForm(FormAction):
     """Example of a custom form action"""
