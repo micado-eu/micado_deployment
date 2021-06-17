bf11a3010a4c9b6b550bdde9110c60a5ff49188e
Luca Gioppo <gioppoluca@libero.it>
2021-06-17 15:01:15 +0200

    fixed rasa training

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,8 +1,15 @@
 from typing import Dict, Text, Any, List, Union, Optional
 
-from rasa_sdk import Tracker
+from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.events import (
+    SlotSet,
+    UserUtteranceReverted,
+    ConversationPaused,
+    EventType,
+    FollowupAction,
+)
 import requests
 import logging
 import os
@@ -19,19 +26,62 @@ handler.setFormatter(formatter)
 root.addHandler(handler)
 logger.addHandler(handler)
 
-class DocumentForm(FormAction):
+class ActionSubmitDocumentForm(Action):
+    def name(self) -> Text:
+        return "action_submit_document_form"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+        """Once we have all the information, attempt to add it to the
+        Google Drive database"""
+
+        import datetime
+
+        document = tracker.get_slot("document")
+
+#        url = "http://10.118.68.78:3000/search_rasa"
+        url = "http://backend:3000/search_rasa"
+
+        logger.info(url)
+
+        params=[('lang','en'), ('words',document)]
+        search_results = requests.get(url, params=params)
+        logger.info(search_results.text)
+        logger.info(search_results.request)
+
+        try:
+            logger.info("trying doing something")
+            logger.info(document)
+            dispatcher.utter_message(search_results.text)
+            return []
+        except Exception as e:
+            logger.error(
+                "Failed to do our things. Error: {}" "".format(e.message),
+                exc_info=True,
+            )
+            dispatcher.utter_message(template="utter_slots_values")
+            return []
+
+
+
+class ValidateDocumentForm(FormValidationAction):
     """Example of a custom form action"""
 
     def name(self) -> Text:
         """Unique identifier of the form"""
+        logger.info("returned the name")
+        return "validate_document_form"
 
-        return "document_form"
-
-    @staticmethod
-    def required_slots(tracker: Tracker) -> List[Text]:
-        """A list of required slots that the form has to fill"""
-
-        return ["document"]
+ #   @staticmethod
+ #   def required_slots(tracker: Tracker) -> List[Text]:
+ #       """A list of required slots that the form has to fill"""
+ #       logger.info("required a slot")#
+#
+#        return ["document"]
 
     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
         """A dictionary to map required slots to
@@ -39,6 +89,8 @@ class DocumentForm(FormAction):
             - intent: value pairs
             - a whole message
             or a list of them, where a first match will be picked"""
+        logger.info("slot_mapping")
+        logger.info(self.from_entity(entity="document", not_intent="chitchat"))
 
         return {
             "document": self.from_entity(entity="document", not_intent="chitchat"),
@@ -53,6 +105,7 @@ class DocumentForm(FormAction):
             "residence permit",
             "stamp duty",
             "identity card",
+            "passport",
         ]
 
 
@@ -66,8 +119,11 @@ class DocumentForm(FormAction):
     ) -> Dict[Text, Any]:
         """Validate document value."""
 
+        logger.info("validate_document in the action")
+        logger.info(value)
         if value.lower() in self.document_db():
             # validation succeeded, set the value of the "document" slot to value
+            logger.info("valid document asked")
             return {"document": value}
         else:
             dispatcher.utter_message(template="utter_wrong_document")
@@ -85,16 +141,16 @@ class DocumentForm(FormAction):
         """Define what the form has to do
             after all required slots are filled"""
         
+        logger.info("in the submit")
+
         lang = "en"
         document = tracker.get_slot("document")
-        print(os.environ['API_HOSTNAME'])
-        print(os.environ['MICADO_API_KEY'])
-        url = "https://"+os.environ['API_HOSTNAME']+"/backend/1.0.0/process_x_doc"
+        url = "https://api.micadoproject.eu/backend/1.0.0/process_x_doc"
 
         logger.info(tracker)
         logger.info(url)
         
-        headers = {"Prefer": "resolution=merge-duplicates","apikey": os.environ['MICADO_API_KEY']}
+        headers = {"Prefer": "resolution=merge-duplicates","apikey": "eyJ4NXQiOiJaalJtWVRNd05USmpPV1U1TW1Jek1qZ3pOREkzWTJJeU1tSXlZMkV6TWpkaFpqVmlNamMwWmc9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImFwcGxpY2F0aW9uIjp7Im93bmVyIjoiYWRtaW4iLCJ0aWVyIjoiVW5saW1pdGVkIiwibmFtZSI6IkRlZmF1bHRBcHBsaWNhdGlvbiIsImlkIjoxLCJ1dWlkIjoiNzBmNGQ5MzEtNmQxYi00ZGJiLThiZGItNjgyNGViZWRhNjA5In0sInRpZXJJbmZvIjp7IlVubGltaXRlZCI6eyJzdG9wT25RdW90YVJlYWNoIjp0cnVlLCJzcGlrZUFycmVzdExpbWl0IjowLCJzcGlrZUFycmVzdFVuaXQiOm51bGx9fSwia2V5dHlwZSI6IlBST0RVQ1RJT04iLCJzdWJzY3JpYmVkQVBJcyI6W3sic3Vic2NyaWJlclRlbmFudERvbWFpbiI6ImNhcmJvbi5zdXBlciIsIm5hbWUiOiJQb3N0Z1JFU1RBUEkiLCJjb250ZXh0IjoiXC9kYlwvdjAxIiwicHVibGlzaGVyIjoiYWRtaW4iLCJ2ZXJzaW9uIjoidjAxIiwic3Vic2NyaXB0aW9uVGllciI6IlVubGltaXRlZCJ9LHsic3Vic2NyaWJlclRlbmFudERvbWFpbiI6ImNhcmJvbi5zdXBlciIsIm5hbWUiOiJtaWNhZG9fYmFja2VuZCIsImNvbnRleHQiOiJcL2JhY2tlbmRcLzEuMC4wIiwicHVibGlzaGVyIjoiYWRtaW4iLCJ2ZXJzaW9uIjoiMS4wLjAiLCJzdWJzY3JpcHRpb25UaWVyIjoiVW5saW1pdGVkIn0seyJzdWJzY3JpYmVyVGVuYW50RG9tYWluIjoiY2FyYm9uLnN1cGVyIiwibmFtZSI6ImNoYXRib3QiLCJjb250ZXh0IjoiXC9ib3RcL3YwMSIsInB1Ymxpc2hlciI6ImFkbWluIiwidmVyc2lvbiI6InYwMSIsInN1YnNjcmlwdGlvblRpZXIiOiJVbmxpbWl0ZWQifV0sImlhdCI6MTU5MjkyMDczMCwianRpIjoiMmZmYzQ3ZTMtMDZkYi00NGIxLWE1NmEtMDhhMjdmOTkyY2JlIn0=.Ud-i7sAF3B915zglltrumtL8TXnojdBEpamYqk1tz4qhzQEyj6UOkrZJELA3C4Czn8x0tFlBEeNz23v6deGs8g_KZRHXrW6mYBH7bRZ-Fbp6yntPhgaUr-e28cTZZlHmOwB0256e1uEFWTnZvjSvomhJHOTi2Zl1fO89So3uHkWA9qYmAU77Z5qz-GlYvDq2G6VTTtYZLIvJ8fkICj53tN_TH6VLHBEIExNtDwV5m4yTMsoxrsHuIuLMNgpczx3ejwk4b81NobJgE2K3Uq7ihKAHo8tZ10qX91q8qDRPVXfjLQE7qTmoOKLPrhXsE3pvuMJyMpESmEn0oKOaphs9Og=="}
         params=[('lang','en'), ('document',document)]
         response_dict = requests.get(url, headers=headers, params=params)
 
